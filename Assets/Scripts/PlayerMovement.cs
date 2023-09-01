af274bd647953b2b4b7583b42cc56fec5ff3a87b
Mateo Astelarra <mateo@astelarra.com>
2023-09-01 13:42:24 -0300

    Revert "Changed Movement"
    This reverts commit 7ff0a7a4c4c3ff69558c376ed8a49a67686f6140.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,95 +4,73 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float scrollingSpeed = 1;
-    [SerializeField] private float forceMagnitud = 20f;
-    [SerializeField] private float dontMoveifTouchclose = 0.3f;
-    [SerializeField] private float speed;
-    [SerializeField] GameObject stick;
+    [SerializeField] private float forceMagnitude = 0.5f;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float dontMoveifTouchclose = 0.5f;
 
     private Camera mainCamera;
     private Rigidbody2D RB;
 
-    private PlayerInput playerInput;
-    private Vector2 input;
-
-    private Vector3 target;
-
-    private bool touchControlsActivated = false;
-
+    private Vector2 movementDirection;
+    private Vector2 lastTouch;
 
     private void Start()
     {
         RB = GetComponent<Rigidbody2D>();
-
-        playerInput = GetComponent<PlayerInput>();
-
+        
         transform.position = new Vector3(0, 0, 0);
         
         mainCamera = Camera.main;
-
-        target = transform.position;
     }
 
     private void Update()
     {
-        if (!touchControlsActivated)
-            UpdateInput();
-        if (Keyboard.current.eKey.wasPressedThisFrame)
-        {
-            touchControlsActivated = !touchControlsActivated;
-            Debug.Log("Touch controls activated/deactivated");
-            stick.SetActive(!touchControlsActivated);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        ConstantFall();
-        if (touchControlsActivated)
-        {
-            UpdateDestiny();
-            return;
-        }   
-        MoveWithGamePad();
-    }
-
-    private void UpdateDestiny()
-    {
-        if ((target - transform.position).sqrMagnitude > dontMoveifTouchclose)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        }
-        target.y += scrollingSpeed * Time.deltaTime;
-    }
-
-    private void ConstantFall()
-    {
-        transform.Translate(0, scrollingSpeed * Time.deltaTime, 0);
-    }
-
-    public void UpdateInput()
-    {
-        input = playerInput.actions["MoveGamepad"].ReadValue<Vector2>();
-    }
-
-    public void MoveWithGamePad()
-    {
-        RB.AddRelativeForce(new Vector2(input.x, input.y) * forceMagnitud, ForceMode2D.Force);
-        Debug.Log(new Vector2(input.x, input.y) * forceMagnitud);
-    }
-
-    public void Move2(InputAction.CallbackContext callbackContext)
-    {
-        if (callbackContext.performed && touchControlsActivated)
+        if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
 
             Vector2 worldPos = mainCamera.ScreenToWorldPoint(touchPos);
 
-            target = new Vector3(worldPos.x, worldPos.y, 0);
+            if (touchPos == lastTouch) 
+            {
+                movementDirection = worldPos - new Vector2(transform.position.x, transform.position.y);
+                if (movementDirection.sqrMagnitude < dontMoveifTouchclose)
+                {
+                    movementDirection = Vector2.zero;
+                    RB.velocity = Vector2.zero;
+                }
+                return; 
+            }
+            
+            lastTouch = touchPos;
 
-            Debug.Log("touch");
+            movementDirection = worldPos - new Vector2(transform.position.x, transform.position.y);
+            movementDirection.Normalize();
+        }
+        else
+        {
+            movementDirection = Vector3.zero;
+            RB.velocity = Vector2.zero;
+        }
+
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            lastTouch = Vector2.zero;
         }
     }
 
+    void FixedUpdate()
+    {
+        transform.Translate(0, scrollingSpeed * Time.deltaTime, 0);
+        if (movementDirection == Vector2.zero)
+        {
+            return;
+        }
+        else
+        {
+            RB.AddForce(movementDirection * forceMagnitude / 50, ForceMode2D.Force);
+
+            RB.velocity = Vector2.ClampMagnitude(RB.velocity, maxVelocity);
+        }
+    }
 }
